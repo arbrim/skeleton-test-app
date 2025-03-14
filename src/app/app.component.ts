fa89abc3e4b8be8db2cef64e9a56d231898c54dd
arbrim <arbrimrashiti@gmail.com>
2025-03-14 19:41:13 +0100

    incorporate issw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import isSW from 'is-sw';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'skeleton-test-app';
+
+  ngOnInit() {
+    console.log('AppComponent initialized');
+
+    // Use the function from your package
+    console.log(isSW("SW"));  // Should print: true
+    console.log(isSW(""));    // Should print: false
+  }
 }
