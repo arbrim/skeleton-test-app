c99076f5c6096b64c584c99791742c213f496d31
arbrim <arbrimrashiti@gmail.com>
2025-03-15 01:37:01 +0100

    testing ngloadingskeleton pacakage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import isSW from 'is-sw';
+import { NgLoadingSkeletonModule } from 'ng-loading-skeleton';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgLoadingSkeletonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
